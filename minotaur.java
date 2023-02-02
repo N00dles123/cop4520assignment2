4f7c47600558ee03d2c2254b26978367b7a07673
Jason Lin <jasonlinfl@gmail.com>
2023-02-01 22:09:39 -0500

    setup program for question 1

---
+++ minotaur.java
@@ -0,0 +1,19 @@
+import java.util.*;
+// make a set number of guests
+// to simulate whether a guest decides to eat the cupcake, we will use random number generation
+
+public class minotaur {
+    public static int numGuests = 10;
+    public static void main(String[] args){
+        
+    }
+}
+
+class guests extends Thread{
+    // no communication between threads during execution communicate before execution
+    // make a strategy for each thread
+    // N is the number of guests and also represents N threads
+    // 
+    minotaur m;
+    
+}
