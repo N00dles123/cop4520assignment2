98e359bccb932ffe67ea857bf4c50acfa7880730
Jason Lin <jasonlinfl@gmail.com>
2023-02-06 22:31:55 -0500

    setup program

--- minotaur.java
+++ minotaur.java
@@ -1,19 +1,28 @@
 import java.util.*;
+import java.util.concurrent.atomic.*;
 // make a set number of guests
-// to simulate whether a guest decides to eat the cupcake, we will use random number generation
-
+// to simulate a guests turn to go find the cupcake since a guest can go multiple times, we will use random number generation
+//
 public class minotaur {
     public static int numGuests = 10;
+    // labrynth will start with 1 cupcake
+    public AtomicBoolean cupcakePresent = new AtomicBoolean(true);
+    // checking if guests ate cupcakes yet
+    // 0 will be false and 1 will be true
+    public AtomicIntegerArray guestEntered = new AtomicIntegerArray(numGuests);
     public static void main(String[] args){
         
     }
 }
 
-class guests extends Thread{
+class guests implements Runnable{
     // no communication between threads during execution communicate before execution
     // make a strategy for each thread
     // N is the number of guests and also represents N threads
     // 
     minotaur m;
     
+    public void run(){
+        
+    }
 }
