4c903f0e91d87b0ede9e569954a226df544a5f17
N00dles123 <jasonlinfl@gmail.com>
2023-02-07 17:37:11 -0500

    minor updates to problem 1

--- minotaur.java
+++ minotaur.java
@@ -1,15 +1,19 @@
 import java.util.*;
 import java.util.concurrent.atomic.*;
+import java.util.concurrent.Semaphore;
 // make a set number of guests
 // to simulate a guests turn to go find the cupcake since a guest can go multiple times, we will use random number generation
-//
+// one thread will be used to manage the cupcakes
+// maintain a global counter to see 
 public class minotaur {
     // you can change the number of guests
     public static int numGuests = 10;
+    public int 
     // labrynth will start with 1 cupcake
     public AtomicBoolean cupcakePresent = new AtomicBoolean(true);
     // checking if guests ate cupcakes yet
     // 0 will be false and 1 will be true
+    public Semaphore lock = new Semaphore(1);
     public AtomicIntegerArray guestEntered = new AtomicIntegerArray(numGuests);
     public static void main(String[] args){
         
@@ -29,6 +33,15 @@ class guests implements Runnable{
     minotaur m;
     
     public void run(){
+        try{
+            // locks piece of code
+            m.lock.acquire();
 
+        } catch (InterruptedException e){
+            System.out.println(e);
+        } finally {
+            // releases lock
+            m.lock.release();
+        }
     }
 }
